9abeb3a525c2810e96902d7cedf43773b5c73590
Kenny <tienkenny96@gmail.com>
2022-10-12 12:38:31 -0700

    Initial commit, basic API and infrastructure

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+#syntax=docker/dockerfile:1
+
+FROM python:3.10-slim-buster
+
+# Copy only requirements to cache them in docker layer
+WORKDIR /code
+COPY ./requirements.txt /code/requirements.txt
+
+# Project initialization:
+RUN pip install -r requirements.txt
+
+# Copy app to Docker image
+COPY app.py /code/
+
+CMD gunicorn app:app -w 2 --threads 2 -b 0.0.0.0:${PORT}
---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from whois_token import WHOIS_API_KEY
+import requests, json
+import os
+
+app = Flask(__name__)
+
+# Route for home page
+@app.route("/")
+def home_page():
+    return render_template("index.html")
+
+# Route for domain query
+@app.route("/<domain>", methods=['GET'])
+def get_details(domain):
+    url = f"https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey={WHOIS_API_KEY}&outputFormat=JSON&domainName={domain}"
+    response = requests.get(url)
+    return (response.json())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
